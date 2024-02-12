977dc2b388d2be7ed43dbc05c3342028e6f761b2
itsThor <itsThor@DESKTOP-798BEVU>
2024-02-12 14:32:51 +0530

    Triangle pattern

---
+++ Java/src/com/java/first/PatternTriangle.java
@@ -0,0 +1,20 @@
+package com.java.first;
+import java.util.Scanner;
+public class PatternTriangle {
+
+	public static void main(String[] args) {
+      Scanner sc= new Scanner(System.in);  
+      System.out.println("Enter the number : ");
+      
+      int n= sc.nextInt();
+      
+      for(int i=0; i<n; i++) {
+    	  for(int j=0; j<i+1; j++) {
+    		  System.out.print(" * ");
+    	  }
+    	  System.out.println();
+      }
+      
+	}
+
+}
