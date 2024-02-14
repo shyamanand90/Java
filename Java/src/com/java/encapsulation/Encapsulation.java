6434df37d95a908a01655f9a27b9545956cb83d5
itsThor <itsThor@DESKTOP-798BEVU>
2024-02-14 10:11:17 +0530

    Encapsulation

---
+++ Java/src/com/java/encapsulation/Encapsulation.java
@@ -0,0 +1,19 @@
+package com.java.encapsulation;
+
+public class Encapsulation {
+	 private int model;
+     private String name;
+     
+	public int getModel() {
+		return model;
+	}
+	public void setModel(int model) {
+		this.model = model;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	} 
+}
---
+++ Java/src/com/java/encapsulation/Execution.java
@@ -0,0 +1,13 @@
+package com.java.encapsulation;
+
+public class Execution {
+    public static void main(String[] args) {
+        
+    	Encapsulation obj=new Encapsulation();
+    	
+    	obj.setName("car1");
+    	obj.setModel(1001);
+    	
+    	System.out.println(obj.getName());
+    }
+}
