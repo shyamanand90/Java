6ef3e73c5569a23bcd470612b961f5b3427a52fd
itsThor <itsThor@DESKTOP-798BEVU>
2024-02-15 14:34:23 +0530

    ops methods

--- Java/src/com/java/strings/StringOps.java
+++ Java/src/com/java/strings/StringOps.java
@@ -4,5 +4,19 @@ public class StringOps {
   
 	public static void main(String[] args) {
 		
+		//Creation of String
+		String name="vijay";
+		//using new keyword in  String class
+		String str=new String(" vijay");
+		
+		
+		//Comparing of strings
+		
+		boolean val=name.equals(str);
+		System.out.println(val);
+		
+		int val2=name.compareTo(str);
+		
+		System.out.print(val2);
 	}
 }
