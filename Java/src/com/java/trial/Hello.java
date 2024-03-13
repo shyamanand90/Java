a5d0e960d282267c2c9ccd836e322a347072aaeb
itsThor <itsThor@DESKTOP-798BEVU>
2024-03-13 18:49:28 +0530

    trial

--- Java/src/com/java/trial/Hello.java
+++ Java/src/com/java/trial/Hello.java
@@ -1,10 +1,37 @@
 package com.java.trial;
+import java.util.* ;
+import java.io.*; 
+import java.util.Scanner;
 
-public class Hello {
+class Hello {
+	
+	public static void main(String args[]) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+	 Scanner sc = new Scanner(System.in);
 
+int basicsal = sc.nextInt();
+char ch = sc.next().charAt(0);
+
+int allow=0;
+
+if(ch=='A'){
+     allow = 1700;
 }
+else if(ch=='B'){
+     allow = 1500;
+}
+else{
+	allow = 1300;
+}
+double hra = (basicsal *20)/100;
+double da = (basicsal *50)/100;
+double pf = (basicsal *11)/100;
+
+double totalsalary =( basicsal + hra + da + allow) - pf;
+
+System.out.print(Math.round(totalsalary));	
+				
+	}
+}
+
